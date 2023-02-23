27cf82317675ee7f2a40800d1e4dda623293c04f
eldesernauta <oscarrojascs@gmail.com>
2023-02-23 16:45:10 +0100

    semana1

--- semana1/test/demo.test.js
+++ semana1/test/demo.test.js
@@ -1,18 +1,23 @@
 const rewire = require('rewire')
 const app = rewire('../src/calculateTravelTime.js')
 
-var calculateTravelTime = app.__get__('calculateTravelTime')
+const calculateTravelTime = app.__get__('calculateTravelTime')
 
 describe('calculateTravelTime', () => {
-    test('Arroja el tiempo de viaje en Avión', () => {
-      expect(calculateTravelTime('Madrid', 'Barcelona', 'plane')).toEqual(1.25);
-    });
-  
-    test('Arroja el tiempo de viaje en Tren', () => {
-      expect(calculateTravelTime('Madrid', 'Barcelona', 'train')).toBe(8.333333333333334);
-    });
-  
-    test('Arroja el tiempo de viaje en Carro', () => {
-      expect(calculateTravelTime('Madrid', 'Barcelona', 'car')).toBeCloseTo(16.67);
-    });
+  test('Arroja el tiempo de viaje en Avión', () => {
+    expect(calculateTravelTime('Madrid', 'Barcelona', 'plane')).toEqual(1.25);
   });
+
+  test('Arroja el tiempo de viaje en Tren', () => {
+    expect(calculateTravelTime('Madrid', 'Barcelona', 'train')).toBe(8.333333333333334);
+  });
+
+  test('Arroja el tiempo de viaje en Carro', () => {
+    expect(calculateTravelTime('Madrid', 'Barcelona', 'car')).toBeCloseTo(16.67);
+  });
+
+  test('Arroja un error cuando se utiliza un medio de transporte erróneo', () => {
+    expect(() => exported('Madrid', 'Barcelona', 'bike')).toThrow();
+  });
+
+});
